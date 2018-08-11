8f26f065dd9c232d9ff25fcf93b6fd713222261a
xumingming <aaaa@gmail.com>
2018-08-11 17:05:23 +0800

    first commit

---
+++ gzhipin-client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// class App extends Component{
+//   render(){
+//     return <div>
+//        Hello react
+//     </div>
+//   }
+// }
+// export default App;
+
+//简单写法 函数形式
+
+export default function App() {
+  return <div>Hello react</div>
+}
---
+++ gzhipin-client/src/index.js
@@ -0,0 +1,8 @@
+/*
+* JS入口文件*/
+import React from 'react'
+import ReactDOM from 'react-dom'
+ import App from './App'
+
+//渲染组件标签到页面
+ ReactDOM.render(<App />,document.getElementById('root'))
